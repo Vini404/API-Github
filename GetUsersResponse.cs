618e3a38421ba9d24b039cb3bc28bfe9942a4e4d
vini404 <vinicius404contato@gmail.com>
2022-09-27 10:34:13 -0300

    commit

---
+++ GetUsersResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace console_api
+{
+    public class GetUsersResponse
+    {
+        public string? login { get; set; }
+        public string? avatar_urlavatar_url { get; set; }
+        public string? url { get; set; }
+        public string? repos_url { get; set; }
+        public bool site_admin { get; set; }
+        public string? location { get; set; }
+        public string? bio { get; set; }
+        public int followers { get; set; }
+        public int following { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using console_api;
+using Newtonsoft.Json;
+using RestSharp;
+
+Console.Write("Nome do usuario: ");
+
+var user = Console.ReadLine();
+
+var userFromGithub = GetUserFromGithub(user);
+
+var jsonToPrint = JsonConvert.SerializeObject(userFromGithub, Formatting.Indented);
+
+Console.WriteLine(jsonToPrint);
+
+GetUsersResponse GetUserFromGithub(string user)
+{
+    var api_url = $"https://api.github.com/users/{user}";
+    var options = new RestClientOptions(api_url)
+    {
+        Timeout = 1000
+    };
+
+    var client = new RestClient(options);
+    var request = new RestRequest();
+
+    var response = client.Execute(request, cancellationToken: new CancellationToken());
+
+    if (response.IsSuccessStatusCode)
+        return System.Text.Json.JsonSerializer.Deserialize<GetUsersResponse>(response.Content);
+
+    throw new Exception("Erro ao buscar usuario");
+}
